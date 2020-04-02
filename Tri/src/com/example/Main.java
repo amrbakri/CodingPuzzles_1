c0f78cc778a0eaa3e1193fd4bd1a49dc4d01f64a
Amr Bakri <amrmb09@yahoo.com>
2020-04-02 13:35:52 +0200

    Update Main.java

--- Tri/src/com/example/Main.java
+++ Tri/src/com/example/Main.java
@@ -3,9 +3,6 @@ package com.example;
 public class Main {
 
 	public static void main(String[] args) {
-		
-//		func();
-//		func_2();
         vacatedMidSecTrig();
 	}
 
@@ -42,78 +39,6 @@ public class Main {
 				System.out.print("  ");
 			else 
 				System.out.print(" *");
-
-		}
-	}
-	
-	private static void func_2() {
-		// TODO Auto-generated method stub
-		
-		for (int i = 0; i <= 4; i++) {
-		
-			for (int s = 0; s <= 5-i-1; s++) {
-				if (s == 0)
-					System.out.print(" ");
-				else 
-					System.out.print("  ");
-			}
-			
-			for (int j = 0; j <i+1; j++) {
-				System.out.print(" *");
-			}
-			System.out.println();
-		}
-		
-		for (int k = 0; k <= 3; k++) {
-			
-			if (k == 0) 
-				System.out.print("*");
-			else 
-				System.out.print(" *");
-			
-			if (k < 2) {
-				for (int l = 0; l <= 0; l++) {
-					System.out.print("  ");
-				}
-			}
-			
-		}
-		
-		
-	}
-
-	private static void func() {
-		// TODO Auto-generated method stub
-		int s = 0;
-		int e = 0;
-		for (int i = 0 ; i <= 4; i++) {
-			
-			for (s = 0; s < i; s++) {
-				if (i > 0) {
-					System.out.print("  ");
-				}
-			} 
-			if (i%2 != 0) {
-				e = 2;
-			} else {
-				e = 1;
-			}
-			for (int j = s; j <= (9-e)-i; j++) {
-				
-				if (i == 0) {
-					System.out.print("* ");
-				} else {
-					if (j >= i) {
-						System.out.print("* ");
-					}
-					if(j == 3 && i%2!=0) {
-						System.out.print("  ");
-					}
-				}
-			}
-			System.out.println();
-		
-		
 		}
 	}
 }
