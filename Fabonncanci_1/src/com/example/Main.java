a745758dd65facdc9b1b1ade60fbbd00b083f248
Amr Bakri <amrmb09@yahoo.com>
2020-04-02 13:32:15 +0200

    Update Main.java

--- Fabonncanci_1/src/com/example/Main.java
+++ Fabonncanci_1/src/com/example/Main.java
@@ -1,24 +1,7 @@
 package com.example;
 
 public class Main {
-
-	static int idx = 0;
-	static int len = 10;
-	static int x[] = new int[len];
-	
-	static boolean inited = false;
-	static int idx_2;
-	static int len_2;
-	static int x_2[] = null;
-	
 	public static void main(String[] args) {
-//		for (int i = 0; i <= len; i++) {
-//			fab();
-//		}
-//		System.out.println();
-
-//		fab_recursive(9);
-//		System.out.println();
 		int res = fab_recursive_2(10);
 		System.out.println(res);
 	}
@@ -33,61 +16,5 @@ public class Main {
 			return fab_recursive_2(kth - 1) + 1;
 		}
 		return fab_recursive_2(kth-1) + fab_recursive_2(kth-2);
-	}
-
-	private static void fab_recursive(int n) {
-		// TODO Auto-generated method stub
-		
-		
-		if (!inited) {
-			idx = 0;
-			len = 10;
-			x = new int[len];
-			inited = true;
-		}
-		if (n == len) {
-			return; 
-		}
-		if (idx == 0) {
-			x[idx] = 0;
-			System.out.print(" " + x[idx]);
-			fab_recursive(++idx);
-		}
-		
-		else if (idx-1 == 0) {
-			x[idx] = x[idx - 1] + 1;
-			System.out.print(", " + x[idx]);
-			fab_recursive(++idx);
-		}
-		
-		else if (idx-1 >= 1) {
-			x[idx] = x[idx - 2] + x[idx - 1];
-			System.out.print(", " + x[idx]);
-			fab_recursive(++idx);
-		}
-	}
-
-	public static void fab() {
-		
-		if (idx == len) {
-			return; 
-		}
-		if (idx == 0) {
-			x[idx] = 0;
-			System.out.print(" " + x[idx]);
-		}
-		
-		else if (idx-1 == 0) {
-			x[idx] = x[idx - 1] + 1;
-			System.out.print(", " + x[idx]);
-		}
-		
-		else if (idx-1 >= 1) {
-			x[idx] = x[idx - 2] + x[idx - 1];
-			System.out.print(", " + x[idx]);
-		}
-		
-		++idx;
-		
 	}
 }
